b4839228fb2ddfb53f8a05c7196539da50573066
tokyohuang123 <tokyohuang123@163.com>
2017-11-24 12:23:13 +0800

    Add files via upload

--- mail.rb
+++ mail.rb
@@ -1,14 +1,67 @@
 require 'mail'  
   
-smtp = { :address => 'smtp.163.com', :port => 25, :domain => '163.com', \
-    :user_name => 'tokyohuang123@163.com', :password => 'XXXXXXXX',\
+smtp = { :address => 'smtp.mxhichina.com', :port => 25, :domain => 'yingfei.onaliyun.com', \
+    :user_name => 'fuwu@yingfei.onaliyun.com', :password => 'Ishangzu365',\
     :enable_starttls_auto => true, :openssl_verify_mode => 'none' }  
 Mail.defaults { delivery_method :smtp, smtp }  
 mail = Mail.new do  
-  from 'tokyohuang123@163.com'  
-  to 'XXXXXg@XXXXXX.com'  
-  subject 'test mail'  
-  body 'body:hello send mail way 2 :)'  
+  from 'fuwu@yingfei.onaliyun.com'  
+  to '766548405@qq.com'  
+
+  subject 'test html mail'  
+  text_part do
+    body 'This is plain text'
+  end
+
+  html_part do
+    content_type 'text/html; charset=UTF-8'
+    body '
+
+<body>
+
+<table border="1" cellspacing="0" cellpadding="0"   >
+<tr bgcolor="#4179E9"  >
+  <td  width="300" colspan="2" align="center" ><font color="#FFEFFF">截止目前云片信息汇总</font></td>
+</tr>
+
+<tr  >
+  <td  width="300">账户剩余条数</td>
+  <td  width="300">%s</td>
+</tr>
+<tr bgcolor="#4169E1" >
+  <td > <font color="#FFFFFF">剩余条数低于该值时提醒</font></td>
+  <td> <font color="#FFFFFF">%s</font></td>
+</tr>
+</table>
+<br>
+<table border="1" cellspacing="0" cellpadding="0" >
+<tr bgcolor="#4169E1"  >
+  <td  width="300" colspan="2" align="center" ><font color="#FFFFFF">%s账单</font></td>
+</tr>
+<tr >
+  <td>计费条数</td>
+  <td>%s</td>
+</tr>
+<tr bgcolor="#4169E1" >
+  <td width="300"><font color="#FFFFFF">产生费用</font></td>
+  <td width="300"><font color="#FFFFFF">%s</font></td>
+</tr>
+<tr >
+  <td>发送成功条数</td>
+  <td>%s</td>
+</tr>
+<tr bgcolor="#4169E1" >
+  <td><font color="#FFFFFF">发送失败条数</font></td>
+  <td><font color="#FFFFFF">%s</font></td>
+</tr>
+<tr>
+  <td>未知短信条数</td>
+  <td>%s</td>
+</tr>
+</table>
+
+</body>'
+  end
   add_file File.expand_path("E:\\ruby\\test.txt")  
 end  
 mail.deliver!  
